5ac2aa770e9979c22eeef0202aeb23aa7163b124
Alexandre Junior <its_alejunior1997@icloud.com>
2026-03-31 10:07:31 -0300

    chore: adjust game list

--- app/(runner-cat)/index.tsx
+++ app/(runner-cat)/index.tsx
@@ -1,36 +1,13 @@
-import { StyleSheet, View, Text, NativeModules } from "react-native";
-// Importe o pacote com o nome que está no package.json do módulo
+// import { requireNativeComponent } from "react-native";
 
-import UnityModule from "unity";
+// const UnityView = requireNativeComponent("UnityView");
 
-export default function CatRunnerScreen() {
-  console.log(UnityModule.hello(), "unity module"); // Verifique se o módulo está sendo importado corretamente
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Meu App com Unity</Text>
-
-      {/* A sua View nativa do iOS renderizada aqui */}
-      <Text>{}</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#f5f5f5",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 20,
-    marginTop: 50,
-  },
-  unityContainer: {
-    width: "100%",
-    flex: 1, // Faz a Unity ocupar o resto da tela
-  },
-});
+// export default function TelaUnity() {
+//   return (
+//     <UnityView
+//       style={{
+//         flex: 1,
+//       }}
+//     />
+//   );
+// }
